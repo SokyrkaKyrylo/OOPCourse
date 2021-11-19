f28af7c61c23c5eec782e6814de5780a73388c0a
SokyrkaKyrylo <kyrylo.sokyrka@valtech.com>
2021-11-19 12:27:41 +0200

    Update Program.cs

--- OOPCourse.Main/Program.cs
+++ OOPCourse.Main/Program.cs
@@ -6,7 +6,7 @@ namespace OOPCourse.Main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello to fantasy city.....");
         }
     }
 }
